1e866791f3cc56fd0f58b730ffa9b55fbd987480
Le Dac Anh <dacanh159@gmail.com>
2022-08-09 18:18:57 +0700

    create schema.sql, data.sql; update docker-compose.yml

---
+++ data.sql
@@ -0,0 +1,167 @@
+INSERT INTO tblUser (name, tel, address, email, username, password, role)
+VALUES ('Anh Quan', '0123456789', 'Viet Nam', 'anhquan@gmail.com', 'anhquan', '12345678', 'ROLE_ADMIN');
+
+INSERT INTO tblUser (name, tel, address, email, username, password, role)
+VALUES ('Dinh Dai', '0123456788', 'Viet Nam', 'dinhdai@gmail.com', 'dinhdai', '12345678', 'ROLE_ADMIN');
+
+INSERT INTO tblUser (name, tel, address, email, username, password, role)
+VALUES ('Viet Anh', '0123456787', 'Viet Nam', 'vietanh@gmail.com', 'vietanh', '12345678', 'ROLE_AGENT');
+
+INSERT INTO tblUser (name, tel, address, email, username, password, role)
+VALUES ('Viet Anh', '0123456786', 'Viet Nam', 'vietanh2@gmail.com', 'vietanh2', '12345678', 'ROLE_AGENT');
+
+INSERT INTO tblUser (name, tel, address, email, username, password, role)
+VALUES ('Dac Anh 1', '0123456785', 'Yen Nghia', 'dacanh@gmail.com', 'dacanh', '12345678', 'ROLE_CUSTOMER');
+
+INSERT INTO tblUser (name, tel, address, email, username, password, role)
+VALUES ('Dac Anh 2', '0123456784', 'Yen Nghia', 'dacanh2@gmail.com', 'dacanh2', '12345678', 'ROLE_CUSTOMER');
+
+INSERT INTO tblUser (name, tel, address, email, username, password, role)
+VALUES ('Dac Anh 3', '0123456783', 'Yen Nghia', 'dacanh3@gmail.com', 'dacanh3', '12345678', 'ROLE_TOUR_GUIDE');
+
+INSERT INTO tblUser (name, tel, address, email, username, password, role)
+VALUES ('Dac Anh 4', '0123456789', 'Yen Nghia', 'dacanh4@gmail.com', 'dacanh4', '12345678', 'ROLE_TOUR_GUIDE');
+
+INSERT INTO tblCity (name, code)
+VALUES ('Ha Noi', 1);
+
+INSERT INTO tblCity (name, code)
+VALUES ('TP Ho Chi Minh', 2);
+
+INSERT INTO tblCity (name, code)
+VALUES ('Hai Phong', 3);
+
+INSERT INTO tblCity (name, code)
+VALUES ('Da Nang', 4);
+
+INSERT INTO tblCity (name, code)
+VALUES ('Cao Bang', 6);
+
+INSERT INTO tblCity (name, code)
+VALUES ('Tuyen Quang', 9);
+
+INSERT INTO tblCity (name, code)
+VALUES ('Thai Nguyen', 12);
+
+INSERT INTO tblCity (name, code)
+VALUES ('Quang Ninh', 17);
+
+INSERT INTO tblCity (name, code)
+VALUES ('Ba Ria - Vung Tau', 52);
+
+INSERT INTO tblTourInfo (name, pricePerPerson, maxSlot, duration, isOneTime, agentId)
+VALUES ('Hà Nội - Yên Tử - Nghỉ Dưỡng Du Thuyền Hạ Long', 50, 10, 4, 0, 3);
+
+INSERT INTO tblTourInfo (name, pricePerPerson, maxSlot, duration, isOneTime, agentId)
+VALUES ('Hà Nội - Sapa - Bản Cát Cát - Fansipan - Ninh Bình - Tràng An - Bái Đính - Tuyệt Tịnh Cốc - Hạ Long - Đảo Titốp - Yên Tử',
+    100, 15, 7, 0, 4);
+
+INSERT INTO tblTourInfo (name, pricePerPerson, maxSlot, duration, isOneTime, agentId)
+VALUES ('Hà Nội - Sapa - Bản Cát Cát - Fansipan - Cổng Trời Ô Quy Hồ', 45, 10, 4, 1, 3);
+
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('SB NỘI BÀI - HÀ NỘI',
+    'Quý khách tập trung tại sân bay Tân Sơn Nhất (ga nội địa), hướng dẫn viên hỗ trợ khách làm thủ tục đáp chuyến bay đi Hà Nội. Đến Sb Nội Bài, xe đưa Quý khách đi ăn tối và ngắm hồ Hoàn Kiếm lung linh trong đêm. Nhận phòng khách sạn nghỉ ngơi.',
+    1, 1, 1
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES (' LĂNG BÁC - YÊN TỬ ',
+    'Quý khách ăn sáng và trả phòng. Xe đưa Quý khách đi tham quan tham quan Lăng Hồ Chủ Tịch. Tiếp tục hành trình xe khởi hành đưa Quý khách đi Yên Tử dùng cơm trưa và tham quan “vùng đất thiêng”:',
+    2, 1, 1
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('HOÀNG HÔN TRÊN VỊNH HẠ LONG',
+    'Thư giãn, thả lỏng mình với các bài tập Yoga nhẹ nhàng dưới sự hướng dẫn của các chuyên gia của khách sạn. Xe khởi hành đón Quý khách đi Hạ Long.',
+    3, 8, 1
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('HẠ LONG – SB NỘI BÀI',
+    ' Xe đón Quý khách đi ăn trưa và mua sắm đặc sản tại Hạ Long. ',
+    4, 8, 1
+    );
+
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('TP.HCM – SB NỘI BÀI – HÀ NỘI',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    1, 1, 2
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('HÀ NỘI – SAPA',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    2, 1, 2
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('SAPA – FANSIPAN LEGEND – ĐÈO Ô QUY HỒ',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    3, 1, 2
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('SAPA – LÀO CAI - NINH BÌNH ',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    4, 1, 2
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('NINH BÌNH - HẠ LONG ',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    5, 1, 2
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('VỊNH HẠ LONG - HANG SỬNG SỐT – ĐẢO TI TỐP',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    6, 1, 2
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('HẠ LONG – YÊN TỬ - SB NỘI BÀI',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    7, 8, 2
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('TP.HCM – SB NỘI BÀI – HÀ NỘI',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    1, 1, 3
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('HÀ NỘI – SAPA',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    2, 1, 3
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('SAPA – FANSIPAN LEGEND – Ô QUY HỒ',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    3, 1, 3
+    );
+INSERT INTO tblDestination (name, content, `day`, cityId, tourInfoId)
+VALUES ('SAPA – LÀO CAI – SB NỘI BÀI',
+    'Quý khách đi Hà Nội nhận phòng khách sạn nghỉ ngơi hoặc tự do đi tham quan Hồ Hoàn Kiếm ngắm bên ngoài Tháp Rùa, Đền Ngọc Sơn, Cầu Thê Húc.',
+    4, 1, 3
+    );
+
+INSERT INTO tblTour(departure, departureTime, tourInfoId, tourGuideId)
+VALUES ('SB. Nội Bài', '2022-08-10 07:00:00', 1, 7);
+
+INSERT INTO tblTour(departure, departureTime, tourInfoId, tourGuideId)
+VALUES ('SB. Nội Bài', '2022-08-24 07:00:00', 1, 8);
+
+INSERT INTO tblTour(departure, departureTime, tourInfoId, tourGuideId)
+VALUES ('SB. Nội Bài', '2022-08-13 07:00:00', 2, 7);
+
+INSERT INTO tblTour(departure, departureTime, tourInfoId, tourGuideId)
+VALUES ('SB. Nội Bài', '2022-08-25 07:00:00', 2, 8);
+
+INSERT INTO tblTour(departure, departureTime, tourInfoId, tourGuideId)
+VALUES ('SB. Nội Bài', '2022-08-14 07:00:00', 3, 7);
+
+INSERT INTO tblBooking(totalPrice, customerId, tourId)
+VALUES (100, 5, 1);
+
+INSERT INTO tblBooking(totalPrice, customerId, tourId)
+VALUES (50, 6, 1);
+
+INSERT INTO tblVisitor(name, bookingId)
+VALUES ('Duong Qua', 1);
+
+INSERT INTO tblVisitor(name, bookingId)
+VALUES ('Co Co', 1);
+
+INSERT INTO tblVisitor(name, bookingId)
+VALUES ('Diep Van', 2);
---
+++ schema.sql
@@ -0,0 +1,90 @@
+CREATE TABLE tblUser (
+    id INTEGER(10) AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    tel CHAR(11) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    dob DATE,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    enable TINYINT DEFAULT 1,
+    role VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE UNIQUE INDEX idx_username
+ON tblUser (username);
+
+CREATE TABLE tblCity (
+    id INTEGER(10) AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    code INTEGER(5),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tblTourInfo (
+    id INTEGER(10) AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    pricePerPerson DOUBLE(10) NOT NULL,
+    maxSlot INTEGER(5) NOT NULL,
+    duration INTEGER(5) NOT NULL,
+    isOneTime TINYINT NOT NULL,
+    agentId INTEGER(10) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (agentId) REFERENCES tblUser(id)
+);
+
+CREATE TABLE tblImageUrl (
+    id INTEGER(10) AUTO_INCREMENT,
+    tourInfoId INTEGER(10) NOT NULL,
+    urlImage VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (tourInfoId) REFERENCES tblTourInfo(id)
+);
+
+CREATE TABLE tblDestination (
+   id INTEGER(10) AUTO_INCREMENT,
+   name VARCHAR(255) NOT NULL,
+   content VARCHAR(1000),
+   `day` INTEGER(10) NOT NULL,
+   cityId INTEGER(10),
+   tourInfoId INTEGER(10),
+   PRIMARY KEY (id),
+   FOREIGN KEY (cityId) REFERENCES tblCity(id),
+   FOREIGN KEY (tourInfoId) REFERENCES tblTourInfo(id)
+);
+
+CREATE TABLE tblTour (
+    id INTEGER(10) AUTO_INCREMENT,
+    departure VARCHAR(255) NOT NULL,
+    departureTime TIMESTAMP NOT NULL,
+    tourInfoId INTEGER(10) NOT NULL,
+    tourGuideId INTEGER(10),
+    PRIMARY KEY (id),
+    FOREIGN KEY (tourInfoId) REFERENCES tblTourInfo(id),
+    FOREIGN KEY (tourGuideId) REFERENCES tblUser(id)
+);
+
+CREATE TABLE tblBooking (
+    id INTEGER(10) AUTO_INCREMENT,
+    totalPrice DOUBLE(10) NOT NULL,
+    status VARCHAR(100) NOT NULL DEFAULT 'CONFIRMING',
+    note VARCHAR(500),
+    review VARCHAR(1000),
+    rating INTEGER(5),
+    customerId INTEGER(10) NOT NULL,
+    tourId INTEGER(10) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (customerId) REFERENCES tblUser(id),
+    FOREIGN KEY (tourId) REFERENCES tblTour(id)
+);
+
+CREATE TABLE tblVisitor (
+    id INTEGER(10) AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    gender VARCHAR(5),
+    dob DATE,
+    bookingId INTEGER(10) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (bookingId) REFERENCES tblBooking(id)
+);
