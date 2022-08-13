e7a6da7f21e510635635acd179fae3a1f303f1f6
TaAnhQuan <77721385+TaAnhQuan@users.noreply.github.com>
2022-08-13 15:49:10 +0700

    user login and register action

---
+++ src/main/java/com/nashintern/travelagent/security/config/WebSecurityConfig.java
@@ -0,0 +1,85 @@
+package com.nashintern.travelagent.security.config;
+
+import com.nashintern.travelagent.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+
+
+//    @Bean(name="myAuthenticationManager")
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .usernameParameter("email")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/doLogout")
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                   @Override
+                   public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+                               Authentication authentication)
+                                 throws IOException, ServletException {
+                       System.out.println("The user " + authentication.getName() + " has logged out");
+                       UrlPathHelper helper = new UrlPathHelper();
+                       String context = helper.getContextPath(request);
+                       response.sendRedirect(context + "/login");
+                   }
+               }
+        ).permitAll();
+        return http.build();
+    }
+}
---
+++ src/main/java/com/nashintern/travelagent/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.nashintern.travelagent.service;
+
+import com.nashintern.travelagent.model.CustomUserDetails;
+import com.nashintern.travelagent.model.User;
+import com.nashintern.travelagent.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.findByEmail(email);
+        if (user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+//        UserDetails userDetails = User.withUsername(user.getEmail())
+//                                    .password(user.getPassword())
+//                                    .authorities("USER").build();
+
+        return new CustomUserDetails(user);
+    }
+}
