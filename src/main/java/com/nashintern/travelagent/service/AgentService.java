dfb16920600c0308b87a9a98b0e046b9bf0ba870
daitran1412 <edogawaconan919@gmail.com>
2022-08-19 00:54:34 +0700

    delete agentController, agentService, create save success, fix agentService

--- src/main/java/com/nashintern/travelagent/service/AgentService.java
+++ src/main/java/com/nashintern/travelagent/service/AgentService.java
@@ -1,9 +1,25 @@
 package com.nashintern.travelagent.service;
 
-import com.nashintern.travelagent.model.Agent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public interface AgentService {
-    Agent getAgentById(int id);
-    void saveAgent(Agent agent);
-    void deleteAgent(int id);
+import com.nashintern.travelagent.model.User;
+import com.nashintern.travelagent.model.AgentDetails;
+import com.nashintern.travelagent.repository.UserRepository;
+@Service
+public class AgentService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.findByEmail(email);
+        if (user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new AgentDetails(user);
+    }
 }
+
